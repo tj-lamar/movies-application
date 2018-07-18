1724c4289e5a8b9f39008518f9a7af184f0ab319
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-07-18 15:02:41 -0500

    Started coding to list all movies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-/**
- * es6 modules and imports
- */
-import sayHello from './hello';
-sayHello('World');
+// /**
+//  * es6 modules and imports
+//  */
+// import sayHello from './hello';
+// sayHello('World');
 
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const $ = require('jquery');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
@@ -15,6 +16,14 @@ getMovies().then((movies) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
+});
+
+$(document).ready(() => {
+
+  $('.container').html('Loading...');
+
+  getMovies().then(res => console.log(res));
+
 });
