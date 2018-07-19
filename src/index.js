2ce73005f253408c035ee3ef848cc980aecbd633
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-07-19 11:35:14 -0500

    Completed "add movies" functionality

--- src/index.js
+++ src/index.js
@@ -8,29 +8,52 @@
  * require style imports
  */
 const {getMovies} = require('./api.js');
-const $ = require('jquery');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
-});
+const showMovies = () => {
+
+    let output = '';
+    getMovies().then(res => {
+        res.forEach(movie => {
+            output += `<li>"${movie.title}"
+  Rating: ${movie.rating}</li>`;
+        });
+        $('.movie-list').html(output);
+    });
+};
 
 $(document).ready(() => {
 
-  $('.movie-list').html('Loading...');
+    $('.movie-list').html('Loading...');
 
-  let output = '';
-  getMovies().then(res => {
-    res.forEach(movie => {
-      output += `<li>"${movie.title}"
-      Rating: ${movie.rating}</li>`
+    getMovies().then((movies) => {
+        console.log('Here are all the movies:');
+        movies.forEach(({title, rating, id}) => {
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
     });
-     $('.movie-list').html(output);
-  });
+
+
+
+    $('#submit').click(e => {
+        const newMovie = { title: $('#moviesInput').val(), rating: $('#ratingsInput').val() };
+        const url = '/api/movies';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newMovie),
+        };
+
+        fetch(url, options)
+            .then(showMovies)
+            .then(() => console.log('SUCCESS'));
+    });
+
+
+    showMovies();
 
 });
