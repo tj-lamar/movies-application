aa82aacd4673a45bdde4e578e8ecab573612bb89
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-08-13 20:34:26 -0500

    Updated look and feel, resume ready

--- src/api.js
+++ src/api.js
@@ -2,5 +2,10 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+  getMoviePoster: (a) => {
+      let title = a.split(" ").join("+");
+      return fetch('http://www.omdbapi.com/?s=' + title + "&apikey=fc8b1f13&")
+          .then(response => response.json());
   }
 };
--- src/index.js
+++ src/index.js
@@ -7,30 +7,64 @@
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const {getMovies, getMoviePoster} = require('./api.js');
 
 const showMovies = () => {
 
-    let bodyOutput = '', dropdownOutput = `<option selected>Select a movie...</option>`;
+    let carouselOutput = '', carouselTicker = '', dropdownOutput = `<option selected>Select a movie...</option>`;
+
+    $('.carousel-inner').html(`<div class="carousel-item active">
+                                <div style="background-color: black; width: 1100px; height: 500px"></div>
+                                </div>`);
+    $('.carousel-indicators').html(`<li data-target="#demo" data-slide-to="0" class="active"></li>`);
+    $('.dropdown').html(`<option selected>Select a movie...</option>`);
+
     getMovies().then(res => {
         res.forEach(movie => {
-            bodyOutput += `<li>"${movie.title}"
-  Rating: ${movie.rating}</li>`;
-            dropdownOutput += `<option value="${movie.id}">${movie.title}</option>`
+            getMoviePoster(movie.title).then(res => {
+                carouselTicker = '';
+                carouselOutput = '';
+                dropdownOutput = '';
+                let posterURL = res.Search[0].Poster;
+  //                     carouselOutput += `<li>"${movie.title}"
+  // Rating: ${movie.rating}</li>`;
+  //               carouselOutput += `<li><img class="d-block w-100" src="${posterURL}" alt="${movie.title}"></li>`;
+                carouselOutput +=
+                    `<div class="carousel-item">
+                     <img class="d-block w-80" src="${posterURL}" alt="${movie.title}">
+                     <div class="carousel-caption d-block">
+                     <h5 class="movie-title">${movie.title}</h5>
+                     ${countStars(movie.rating)}
+                     </div></div>`;
+                carouselTicker += `<li data-target="#demo" data-slide-to="${movie.id}"></li>`;
+                dropdownOutput += `<option value="${movie.id}">${movie.title}</option>`;
+                $('.carousel-inner').append(carouselOutput);
+                $('.carousel-indicators').append(carouselTicker);
+                $('.dropdown').append(dropdownOutput);
+            }).catch(e => console.log(e));
         });
-        $('.movie-list').html(bodyOutput);
-        $('.dropdown').html(dropdownOutput);
     });
 };
 
+
+const countStars = rating => {
+    let output = `<p class="stars">`;
+    for (let i = 0; i < rating; i++) {
+        output += `<i class="fas fa-star fa-md"></i>`;
+    }
+    output += `</p>`;
+    return output;
+};
+
+
 $(document).ready(() => {
 
-    $('.movie-list').html('Loading...');
+    $('.carousel-inner').text('Loading...');
 
     getMovies().then((movies) => {
         console.log('Here are all the movies:');
         movies.forEach(({title, rating, id}) => {
-            console.log(`id#${id} - ${title} - rating: ${rating}`);
+            console.log(`${id} - ${title} - rating: ${rating}`);
         });
     }).catch((error) => {
         alert('Oh no! Something went wrong.\nCheck the console for details.');
